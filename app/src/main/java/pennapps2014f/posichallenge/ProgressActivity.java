169d1f8cf5ce02d608d0a7e5ac8bca17b21ca2c3
Daven Wu <epikglow@gmail.com>
2014-09-12 23:32:52 -0400

    Added dependencies for CalDroid (calendar) and Android Support Library v7 Added base calendar view to ProgressActivity

---
+++ app/src/main/java/pennapps2014f/posichallenge/ProgressActivity.java
@@ -0,0 +1,53 @@
+package pennapps2014f.posichallenge;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.roomorama.caldroid.CaldroidFragment;
+
+import java.util.Calendar;
+
+
+public class ProgressActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        CaldroidFragment caldroidFragment = new CaldroidFragment();
+        Bundle args = new Bundle();
+        Calendar cal = Calendar.getInstance();
+        args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
+        args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+        caldroidFragment.setArguments(args);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.calendarContainer, caldroidFragment);
+        transaction.commit();
+
+        setContentView(R.layout.activity_progress);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.progress, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
