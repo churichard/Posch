384e08f008ed0e090b1c02c826d329470c22db3e
Daven Wu <epikglow@gmail.com>
2014-09-13 11:29:35 -0400

    Switched from simplediskcache to SharedPreferences

--- app/src/main/java/pennapps2014f/posichallenge/MainActivity.java
+++ app/src/main/java/pennapps2014f/posichallenge/MainActivity.java
@@ -1,6 +1,11 @@
 package pennapps2014f.posichallenge;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.Typeface;
@@ -10,16 +15,23 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.File;
-import java.io.IOException;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Map;
 import java.util.Random;
 
-import cz.fhucho.android.util.SimpleDiskCache;
-
 public class MainActivity extends Activity {
-    public static SimpleDiskCache dateCache;
-    private static final int DISK_CACHE_SIZE = 1024 * 1024 * 5; // 5 MB
+    // Storage of dates and completions/incompletions
+    public static SharedPreferences dateStorage;
+    public static SharedPreferences.Editor editor;
+
+    // Alarm manager
+    private AlarmManager alarmManager;
+    private PendingIntent alarmIntent;
 
     // Resources
     Resources res;
@@ -42,25 +54,34 @@ public class MainActivity extends Activity {
         // Initialization
         randomGen = new Random();
         res = getResources();
+        dateStorage = getPreferences(Context.MODE_PRIVATE);
+        editor = dateStorage.edit();
+        alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, DateChangeReceiver.class);
+        alarmIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
 
-        // Create/open cache
-        try {
-            File cacheDir = getFilesDir();
-            dateCache = SimpleDiskCache.open(cacheDir, 1, DISK_CACHE_SIZE);
-        } catch (IOException e) {
-            e.printStackTrace();
+        // Redisplay complete/incomplete challenges
+        for(Map.Entry<String, ?> entry : dateStorage.getAll().entrySet()) {
+            Date date = Date.valueOf(entry.getKey());
+
+            if((entry.getValue()).equals("complete")) {
+                ProgressActivity.setDateComplete(date);
+            } else if((entry.getValue()).equals("incomplete")) {
+                ProgressActivity.setDateIncomplete(date);
+            }
         }
+
+        // Set to broadcast to DateChangeReceiver every midnight
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        alarmManager.setRepeating(AlarmManager.RTC, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, alarmIntent);
 
         // Sets challenge and background color
         setText();
         setColor();
-
-        final Button button1 = (Button) findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Does some stuff
-            }
-        });
 
         final Button button2 = (Button) findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
@@ -94,10 +115,17 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
-        MenuItem item= menu.findItem(R.id.action_settings);
+        MenuItem item = menu.findItem(R.id.action_settings);
         item.setVisible(false);
         super.onPrepareOptionsMenu(menu);
         return false;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        editor.commit();
     }
 
     // Generates a random challenge to display
@@ -127,5 +155,19 @@ public class MainActivity extends Activity {
         int randNum = randomGen.nextInt(colors.length);
         // Set the background color
         root.setBackgroundColor(Color.parseColor(colors[randNum]));
+    }
+
+    public void viewProgress(View v) {
+        Intent intent = new Intent(this, ProgressActivity.class);
+        startActivity(intent);
+    }
+
+    public void completeChallenge(View v) {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date today = new Date(System.currentTimeMillis());
+
+        editor.putString(dateFormat.format(today), "complete");
+        ProgressActivity.setDateComplete(today);
+        Toast.makeText(this, "Good job! Keep being positive!", Toast.LENGTH_LONG).show();
     }
 }
